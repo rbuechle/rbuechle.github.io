2cc0c8a507458c05c2628ca41cc69fe582e95759
Becca <rbuechle@my365.bellevue.edu>
2020-05-27 22:59:56 -0500

    finishing touches

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let all = document.getElementsByTagName("*");
 let darkEnabled = false;
 let btn = document.getElementById("lightSwitch");
-btn.onclick = function() {
+function darkMode() {
   if(!darkEnabled){
       for (var i=0, max=all.length; i < max; i++) {
           all[i].classList.add('darkMode');
