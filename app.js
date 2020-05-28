f8b572984595b69f3bc9c7baf81a498416eac829
Becca <rbuechle@my365.bellevue.edu>
2020-05-27 22:53:45 -0500

    final touches

---
+++ app.js
@@ -0,0 +1,17 @@
+let all = document.getElementsByTagName("*");
+let darkEnabled = false;
+let btn = document.getElementById("lightSwitch");
+btn.onclick = function() {
+  if(!darkEnabled){
+      for (var i=0, max=all.length; i < max; i++) {
+          all[i].classList.add('darkMode');
+      }
+      darkEnabled = true;
+  }
+  else {
+    for (var i=0, max=all.length; i < max; i++) {
+        all[i].classList.remove('darkMode');
+    }
+    darkEnabled = false;
+  }
+}
